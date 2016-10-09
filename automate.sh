a7e9b039492006d95a8f24448c77748172225651
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-10 03:54:10 +0530

    added logs flag

--- automate.sh
+++ automate.sh
@@ -1,5 +1,5 @@
 echo 'Generating Lexer and Parser'
-java -jar /usr/local/lib/antlr-4.5.3-complete.jar newton.g4 -o src/ -listener -no-visitor
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar newton.g4 -o src/ -listener -no-visitor -long-messages
 cd src/
 javac Renderer.java -d ../gen
 echo 'Compiling Renderer'
