12ce642cf34ab20821f8a7d4127df5fe0fd28db0
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-09 19:40:06 +0530

    replaced alias with full path

--- automate.sh
+++ automate.sh
@@ -1,5 +1,5 @@
 echo 'Generating Lexer and Parser'
-antlr4 newton.g4 -o src/ -listener -no-visitor
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar newton.g4 -o src/ -listener -no-visitor
 cd src/
 javac Renderer.java -d ../gen
 echo 'Compiling Renderer'
