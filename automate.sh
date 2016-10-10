703ec7961d7f10a49539dcd949f5f84db64cb925
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-10 19:37:36 +0530

    using test file by default

--- automate.sh
+++ automate.sh
@@ -1,5 +1,5 @@
 echo 'Generating Lexer and Parser'
-java -jar /usr/local/lib/antlr-4.5.3-complete.jar newton.g4 -o src/ -listener -no-visitor -long-messages
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar newton.g4 -o src/ -listener -no-visitor -long-messages -Werror
 cd src/
 javac Renderer.java -d ../gen
 echo 'Compiling Renderer'
@@ -13,15 +13,19 @@ then
 elif [ $1 == 'ast' ]
 then
     echo 'Running TestRig...'
-    java org.antlr.v4.gui.TestRig newton file -gui
+    java org.antlr.v4.gui.TestRig newton file -gui ../testing/test.newton
 elif [ $1 == 'token' ]
 then
     echo 'Running TestRig...'
-    java org.antlr.v4.gui.TestRig newton file -tokens
+    java org.antlr.v4.gui.TestRig newton file -tokens ../testing/test.newton
 elif [ $1 == 'trace' ]
 then
     echo 'Running TestRig...'
-    java org.antlr.v4.gui.TestRig newton file -trace
+    java org.antlr.v4.gui.TestRig newton file -trace ../testing/test.newton
+elif [ $1 == 'diagnostics' ]
+then
+    echo 'Running TestRig...'
+    java org.antlr.v4.gui.TestRig newton file -diagnostics ../testing/test.newton
 else
-    echo 'Enter build or ast'
+    echo 'Enter build / ast / token / trace / diagnostics '
 fi
